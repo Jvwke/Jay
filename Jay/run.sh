3e041bbd51113a27a447c7f79baec629feac4fff
james.zhang <james.zhang@otms.com>
2018-05-04 21:40:16 +0800

    enhance run.sh

--- Jay/run.sh
+++ Jay/run.sh
@@ -1,6 +1,22 @@
-cd target/classes
+if [ ! -n "$1" ];then
+    echo "Please specify the program file, e.g. 'run.sh /xxx_dir/xxxx.jay'"
+    exit 1
+fi
 
+file=$1
+if [ "${file:0:2}" == "./" ]; then
+    path=$(dirname $(readlink -f $0)) # the current directory of run.sh
+    file=$path${file:1} # replace './' with '/'
+fi
+
+if [ ! -f $file ];then
+    echo "Program file is not found"
+    exit 1
+fi
+
+cd target/classes
 echo 'run file '$1
-java com.jay.Jay $1
+java com.jay.Jay $file
 
 cd ../../
+
