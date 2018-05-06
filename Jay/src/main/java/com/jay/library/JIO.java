efd418287c75f7b6336711a77c2c1c1f4564f0ea
james.zhang <james.zhang@otms.com>
2018-05-06 22:57:17 +0800

    change println

--- Jay/src/main/java/com/jay/library/JIO.java
+++ Jay/src/main/java/com/jay/library/JIO.java
@@ -1,24 +1,29 @@
 package com.jay.library;
 
+import java.util.Arrays;
+
 import com.jay.type.JValue;
 
 public class JIO {
 
     public static JValue native_print(JValue... values) {
-        if(values == null || values.length == 0) {
+        if (values == null || values.length == 0) {
             System.out.print("");
         } else {
             int len = values.length;
-            for(int i = 0; i< len;i++) {
-                System.out.print(values[i].toString());
-                if(len > 1 && i < len -1) {
-                    System.out.print(", ");
+            if (len == 1) {
+                System.out.print(values[0].toString());
+            } else {
+                String tpl = values[0].toString();
+                for (int i = 1; i < len; i++) {
+                    tpl = tpl.replace("{" + (i - 1) + "}", values[i].toString());
                 }
+                System.out.print(tpl);
             }
         }
         return null;
     }
-    
+
     public static JValue native_println(JValue... values) {
         native_print(values);
         System.out.println();
