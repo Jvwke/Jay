4e39b923c217bf449456ade4fb7760befbae2c94
james.zhang <james.zhang@otms.com>
2018-05-07 21:10:33 +0800

    optmize

--- Jay/src/main/java/com/jay/library/JIO.java
+++ Jay/src/main/java/com/jay/library/JIO.java
@@ -1,10 +1,14 @@
 package com.jay.library;
 
-import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import com.jay.type.JString;
 import com.jay.type.JValue;
 
 public class JIO {
+
+    private static final Pattern PARAM_PATTERN = Pattern.compile("\\{(\\d+)\\}");
 
     public static JValue native_print(JValue... values) {
         if (values == null || values.length == 0) {
@@ -15,10 +19,27 @@ public class JIO {
                 System.out.print(values[0].toString());
             } else {
                 String tpl = values[0].toString();
-                for (int i = 1; i < len; i++) {
-                    tpl = tpl.replace("{" + (i - 1) + "}", values[i].toString());
+                Matcher m = PARAM_PATTERN.matcher(tpl);
+                if (m.find()) {
+                    int start = 0, end = 0;
+                    while (m.find(start)) {
+                        start = m.start();
+                        System.out.print(tpl.subSequence(end, start));
+                        end = m.end();
+                        int index = Integer.valueOf(m.group(1));
+                        if(index + 1 < values.length) {
+                            System.out.print(values[index + 1].toString());
+                        } else {
+                            System.out.print(m.group());
+                        }
+                        start = end;
+                    }
+                    if(end + 1 < tpl.length()) {
+                        System.out.print(tpl.substring(end + 1));
+                    }
+                } else {
+                    System.out.print(tpl);
                 }
-                System.out.print(tpl);
             }
         }
         return null;
@@ -28,5 +49,10 @@ public class JIO {
         native_print(values);
         System.out.println();
         return null;
+    }
+    
+    public static void main(String[] args) {
+        JValue[] values = new JValue[] {new JString("{0}xxx{1}yy{4}y{2}{3}"), new JString("a"), new JString("b"), new JString("c")};
+        JIO.native_print(values);
     }
 }
